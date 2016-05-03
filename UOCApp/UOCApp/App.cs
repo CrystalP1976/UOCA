e32ab91d0234171803429ab3f04af3347d613488
XCVG <xcvgvcx@gmail.com>
2016-05-03 12:25:32 -0700

    added splash page

--- UOCApp/UOCApp/App.cs
+++ UOCApp/UOCApp/App.cs
@@ -11,18 +11,8 @@ namespace UOCApp
 	{
 		public App ()
 		{
-			// The root page of your application
-			MainPage = new ContentPage {
-				Content = new StackLayout {
-					VerticalOptions = LayoutOptions.Center,
-					Children = {
-						new Label {
-							XAlign = TextAlignment.Center,
-							Text = "Welcome to Xamarin Forms!"
-						}
-					}
-				}
-			};
+            // The root page of your application
+            MainPage = new UOCApp.SplashPage();
 		}
 
 		protected override void OnStart ()
---
+++ UOCApp/UOCApp/SplashPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace UOCApp
+{
+	public partial class SplashPage : ContentPage
+	{
+		public SplashPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
