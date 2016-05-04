e862e8404f765ce2c6bb5509adaeb2377fae87bf
XCVG <xcvgvcx@gmail.com>
2016-05-04 12:36:09 -0700

    updated app info

--- UOCApp/UOCApp.Droid/MainActivity.cs
+++ UOCApp/UOCApp.Droid/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace UOCApp.Droid
 {
-	[Activity (Label = "UOCApp", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	[Activity (Label = "Ultimate Obstacle Course", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
 	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
 	{
 		protected override void OnCreate (Bundle bundle)
