9091a21b2f49219667fbce827b8868260ecfa726
XCVG <xcvgvcx@gmail.com>
2016-05-10 13:44:06 -0700

    added a very simple GET

---
+++ UOCApp/UOCApp/Models/AdminResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UOCApp.Models
+{
+    class AdminResult
+    {
+        public int result_id { get; set; }
+        public string student_name { get; set; }
+        public string date { get; set; }
+        public string time { get; set; }
+
+        //TODO move the get operation here
+
+    }
+}
