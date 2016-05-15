efde91873ba991c966719426c08c7fd7efa72fac
XCVG <xcvgvcx@gmail.com>
2016-05-15 13:22:26 -0700

    it works again now

---
+++ UOCApp/UOCApp/Models/Result.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace UOCApp.Models
+{
+    [Table("result")]
+    class Result
+    {
+        [PrimaryKey,AutoIncrement]
+        public int result_id { get; set; }
+        public string date { get; set; }
+        public decimal time { get; set; }
+        public int ranked { get; set; }
+        public string student_name { get; set; }
+        public string student_gender { get; set; }
+        public int student_grade { get; set; }
+
+        public override string ToString()
+        {
+            return (base.ToString() + " [" + result_id + "," + date + "," + time + "," + ranked + "," + student_name + "," + student_gender + "," + student_grade + "]");
+        }
+    }
+}
