b42002b07be7a640aeb0d93ab05e38594c83598b
XCVG <xcvgvcx@gmail.com>
2016-05-17 12:29:53 -0700

    building the skeleton of what will become the local results page

---
+++ UOCApp/UOCApp/Models/PrivateResult.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UOCApp.Models
+{
+    class PrivateResult
+    {
+
+
+        public int result_id { get; set; }
+        public string student_name { get; set; }
+        public string date { get; set; }
+        public string time { get; set; }
+        public string school_name { get; set; }
+        public string sortableDate { get; set; }
+        public double sortableTime { get; set; }
+    }
+}
--- UOCApp/UOCApp/TimesPage.xaml.cs
+++ UOCApp/UOCApp/TimesPage.xaml.cs
@@ -1,19 +1,48 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using UOCApp.Models;
 using Xamarin.Forms;
 
 namespace UOCApp
 {
 	public partial class TimesPage : ContentPage
 	{
-		public TimesPage ()
+        List<PrivateResult> baseResults;
+        ObservableCollection<PrivateResult> results; 
+
+        public TimesPage ()
 		{
 			InitializeComponent ();
-		}
+
+            baseResults = new List<PrivateResult>();
+            results = new ObservableCollection<PrivateResult>();
+
+            ListViewTimes.ItemsSource = results;
+
+            results.Add(new PrivateResult { result_id = 5, student_name = "John Doe", date = "April 28 2016", time = "11:11.111" });
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            //TODO initial get results
+
+        }
+
+        private void CopyResults()
+        {
+            this.results.Clear();
+
+            foreach (PrivateResult result in this.baseResults)
+            {
+                this.results.Add(result);
+            }
+        }
 
         private void NavHome(object sender, EventArgs args)
         {
@@ -31,6 +60,24 @@ namespace UOCApp
         {
             Console.WriteLine("Clicked Nav Admin");
             Navigation.PushAsync(new AdminPage());
+        }
+
+        private async void ButtonDeleteClick(object sender, EventArgs args)
+        {
+            //TODO
+
+        }
+
+        //Fired when any filter is changed, refilters the list
+        private void FilterChange(object sender, EventArgs args)
+        {
+            //TODO
+        }
+
+        //Fired when the sort is changed, resorts the list
+        private void SortChange(object sender, EventArgs args)
+        {
+            //TODO
         }
     }
 }
